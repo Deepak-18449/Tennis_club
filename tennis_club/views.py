6a4fbc13dabc7bb94829ee1ed5d3f0d9b628599d
Deepak-18449 <deepakaswani2004@gmail.com>
2025-01-11 19:28:06 +0530

    htmlpage

--- tennis_club/views.py
+++ tennis_club/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseNotFound
+from django.template.loader import render_to_string
 
 # Create your views here.
 def index(request):
-    return HttpResponse("welcome home page")
+    # return HttpResponse("welcome home page")
+    return HttpResponse(render_to_string('tennis/tennis.html'))
 
 def index2(request):
     return HttpResponse("welcome tennnis page")
